6f31a6bd57c7f1b886160bcb9ad51764f17dcedc
YANG XIMING <yxm1010122195@gmail.com>
2024-03-05 22:36:10 +0800

    Initial commit

---
+++ src/main/java/com/rls/rls/RlsApplication.java
@@ -0,0 +1,13 @@
+package com.rls.rls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rls/rls/RlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rls.rls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
